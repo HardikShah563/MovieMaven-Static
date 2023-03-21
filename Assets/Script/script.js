1303578f63257ead7c85db3a46ecf921fc81ebe5
HardikShah563 <hardikts@gmail.com>
2023-03-21 22:02:38 +0530

    file names changed

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,17 +1,20 @@
-let editVenue = document.querySelectorAll(".editVenue");
-let deleteVenue = document.querySelectorAll(".deleteVenue");
-editVenue.forEach(element => {
-    element.addEventListener("click", () => {
-        console.log("Edit Venue been clicked");
-    })
-});
+// let editVenue = document.querySelectorAll(".editVenue");
+// let deleteVenue = document.querySelectorAll(".deleteVenue");
+// let popupModal = document.querySelector(".popup-modal");
+// editVenue.forEach(element => {
+//     element.addEventListener("click", () => {
+//         popupModal.classList.add("view-block");
+//         console.log("Edit Venue been clicked");
+//     })
+// });
 
-deleteVenue.forEach(element => {
-    element.addEventListener("click", () => {
-        console.log("Delete Venue has been clicked");
-    })
-});
+// deleteVenue.forEach(element => {
+//     element.addEventListener("click", () => {
+//         console.log("Delete Venue has been clicked");
+//     })
+// });
 
-// editVenue[0].addEventListener("click", () => {
-//     console.log("I've been clicked");
-// })
+// let closeMark = document.querySelector(".closeMark");
+// closeMark.addEventListener("click", () => {
+//     popupModal.classList.remove("view-block");
+// });
