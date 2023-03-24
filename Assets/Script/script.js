99ddcb5ea2be62dbc149ad2d01f9ca1a6d24a144
HardikShah563 <hardikts@gmail.com>
2023-03-24 21:39:20 +0530

    user dashboard created

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -19,6 +19,10 @@
 //     popupModal.classList.remove("view-block");
 // });
 
+// ==========================
+// COST CALCULATE FOR TICKETS
+// ==========================
+
 // Code for Ticket with cost 349
 let plus1 = document.getElementById("plus1");
 let minus1 = document.getElementById("minus1");
@@ -29,7 +33,7 @@ plus1.addEventListener("click", () => {
     document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) + 349;
 });
 minus1.addEventListener("click", () => {
-    if(+(document.getElementById("seats1").value) != 0){
+    if (+(document.getElementById("seats1").value) != 0) {
         document.getElementById("seats1").value = +(document.getElementById("seats1").value) - 1;
         document.getElementById("ticketCost-1").value = +(document.getElementById("ticketCost-1").value) - 349;
         document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) - 349;
@@ -45,7 +49,7 @@ plus2.addEventListener("click", () => {
     document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) + 199;
 });
 minus2.addEventListener("click", () => {
-    if(+(document.getElementById("seats2").value) != 0){
+    if (+(document.getElementById("seats2").value) != 0) {
         document.getElementById("seats2").value = +(document.getElementById("seats2").value) - 1;
         document.getElementById("ticketCost-2").value = +(document.getElementById("ticketCost-2").value) - 199;
         document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) - 199;
@@ -61,9 +65,26 @@ plus3.addEventListener("click", () => {
     document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) + 149;
 });
 minus3.addEventListener("click", () => {
-    if(+(document.getElementById("seats3").value) != 0){
+    if (+(document.getElementById("seats3").value) != 0) {
         document.getElementById("seats3").value = +(document.getElementById("seats3").value) - 1;
         document.getElementById("ticketCost-3").value = +(document.getElementById("ticketCost-3").value) - 149;
         document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) - 149;
     }
 });
+
+// ==========================
+// RATING SYSTEM
+// ==========================
+const btn = document.querySelector("button");
+const post = document.querySelector(".post");
+const widget = document.querySelector(".star-widget");
+const editBtn = document.querySelector(".edit");
+btn.onclick = () => {
+    widget.style.display = "none";
+    post.style.display = "block";
+    editBtn.onclick = () => {
+        widget.style.display = "block";
+        post.style.display = "none";
+    }
+    return false;
+}
