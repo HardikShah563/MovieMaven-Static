31399027e7222c9411f93241474b9faac20c31d7
HardikShah563 <hardikts@gmail.com>
2023-03-24 08:16:19 +0530

    basic layout of bookShow page created

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -18,3 +18,52 @@
 // closeMark.addEventListener("click", () => {
 //     popupModal.classList.remove("view-block");
 // });
+
+// Code for Ticket with cost 349
+let plus1 = document.getElementById("plus1");
+let minus1 = document.getElementById("minus1");
+
+plus1.addEventListener("click", () => {
+    document.getElementById("seats1").value = +(document.getElementById("seats1").value) + 1;
+    document.getElementById("ticketCost-1").value = +(document.getElementById("ticketCost-1").value) + 349;
+    document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) + 349;
+});
+minus1.addEventListener("click", () => {
+    if(+(document.getElementById("seats1").value) != 0){
+        document.getElementById("seats1").value = +(document.getElementById("seats1").value) - 1;
+        document.getElementById("ticketCost-1").value = +(document.getElementById("ticketCost-1").value) - 349;
+        document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) - 349;
+    }
+});
+// Code for Ticket with cost 199
+let plus2 = document.getElementById("plus2");
+let minus2 = document.getElementById("minus2");
+
+plus2.addEventListener("click", () => {
+    document.getElementById("seats2").value = +(document.getElementById("seats2").value) + 1;
+    document.getElementById("ticketCost-2").value = +(document.getElementById("ticketCost-2").value) + 199;
+    document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) + 199;
+});
+minus2.addEventListener("click", () => {
+    if(+(document.getElementById("seats2").value) != 0){
+        document.getElementById("seats2").value = +(document.getElementById("seats2").value) - 1;
+        document.getElementById("ticketCost-2").value = +(document.getElementById("ticketCost-2").value) - 199;
+        document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) - 199;
+    }
+});
+// Code for Ticket with cost 149
+let plus3 = document.getElementById("plus3");
+let minus3 = document.getElementById("minus3");
+
+plus3.addEventListener("click", () => {
+    document.getElementById("seats3").value = +(document.getElementById("seats3").value) + 1;
+    document.getElementById("ticketCost-3").value = +(document.getElementById("ticketCost-3").value) + 149;
+    document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) + 149;
+});
+minus3.addEventListener("click", () => {
+    if(+(document.getElementById("seats3").value) != 0){
+        document.getElementById("seats3").value = +(document.getElementById("seats3").value) - 1;
+        document.getElementById("ticketCost-3").value = +(document.getElementById("ticketCost-3").value) - 149;
+        document.getElementById("totAmount").value = +(document.getElementById("totAmount").value) - 149;
+    }
+});
